e133e2dad046da0c502ac956ef08dcd895855427
rruebner <rruebner@gmail.com>
2022-06-27 22:00:09 -0400

    added header div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import "./styles.css"
+
+
 console.log("stesttt");
+
+const content = document.getElementById("content");
+const element = document.createElement('div');
+element.classList.add('header');
+element.textContent = "LOL";
+content.appendChild(element);
