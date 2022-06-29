329eeb719ad9549d789a301aa8aeeb694ae57b2c
rruebner <rruebner@gmail.com>
2022-06-29 19:15:33 -0400

    making main page content module

--- src/modules/mainpage.js
+++ src/modules/mainpage.js
@@ -1,2 +1,30 @@
 
 
+function render() {
+
+    const content = document.getElementById("content");
+
+    const main = document.createElement('div');
+    main.classList.add('main');
+    
+    const textbox = document.createElement('div');
+    textbox.classList.add('textbox');
+    const name = document.createElement('h1');
+    name.classList.add('name');
+    name.textContent = "Le Restaurant";
+
+    const copy = document.createElement('p');
+    copy.classList.add('copy');
+    copy.textContent = "lorem ipsum iuetgheuoighe eoigjetrog egeg gtrg";
+
+
+
+    textbox.appendChild(name);
+    textbox.appendChild(copy);
+
+    main.appendChild(textbox);
+    content.appendChild(main);
+
+}
+
+export { render as renderMain };
