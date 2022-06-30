3a1182f753b30bca9ac8e1d890bdb23f95100f65
rruebner <rruebner@gmail.com>
2022-06-29 20:22:45 -0400

    added structure for all 4 nav bar buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {renderHeader} from "./modules/header.js";
 import {renderFooter} from "./modules/footer.js";
 import {renderMain} from "./modules/mainpage.js";
 import {renderAbout} from "./modules/about.js";
+import {renderMenu} from "./modules/menu.js";
+import {renderContact} from "./modules/contact.js";
 
 window.addEventListener('load', function() {
 renderHeader();
@@ -13,14 +15,35 @@ renderFooter();
 const content = document.getElementById("content");
 
 
-
+// check for clicks on header buttons
 window.addEventListener('click', function(e){
     if (e.target.classList.contains('button')) {
+        if (e.target.textContent === "Home"){
+            Home();
+        }
         if (e.target.textContent === "About"){
             About();
         }
+        if (e.target.textContent === "Menu"){
+            Menu();
+        }
+        if (e.target.textContent === "Contact"){
+            Contact();
+        }
     }
 });
+
+// Load the Home page
+function Home(){
+    // clear DOM
+    while (content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    // remake DOM with About page
+    renderHeader();
+    renderMain();
+    renderFooter();
+}
 
 // Load the About page
 function About(){
@@ -33,6 +56,32 @@ function About(){
     renderAbout();
     renderFooter();
 }
+
+// Load the Menu page
+function Menu(){
+    // clear DOM
+    while (content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    // remake DOM with About page
+    renderHeader();
+    renderMenu();
+    renderFooter();
+}
+
+// Load the Menu page
+function Contact(){
+    // clear DOM
+    while (content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    // remake DOM with About page
+    renderHeader();
+    renderContact();
+    renderFooter();
+}
+
+
 
 
 function makeElement(type, classaray, text){
