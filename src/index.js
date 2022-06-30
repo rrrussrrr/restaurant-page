af4e98ceffdb5af96b865f2e34fd09abffde2ca2
rruebner <rruebner@gmail.com>
2022-06-29 20:15:59 -0400

    added functionality to buttons to change to about page (more to come)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import "./styles.css"
+import "./styles.css";
+import {renderHeader} from "./modules/header.js";
+import {renderFooter} from "./modules/footer.js";
+import {renderMain} from "./modules/mainpage.js";
+import {renderAbout} from "./modules/about.js";
 
-
-console.log("stesttt");
+window.addEventListener('load', function() {
+renderHeader();
+renderMain();
+renderFooter();
+});
 
 const content = document.getElementById("content");
-const element = document.createElement('div');
-element.classList.add('header');
-element.textContent = "LOL";
-content.appendChild(element);
+
+
+
+window.addEventListener('click', function(e){
+    if (e.target.classList.contains('button')) {
+        if (e.target.textContent === "About"){
+            About();
+        }
+    }
+});
+
+// Load the About page
+function About(){
+    // clear DOM
+    while (content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    // remake DOM with About page
+    renderHeader();
+    renderAbout();
+    renderFooter();
+}
+
+
+function makeElement(type, classaray, text){
+    const element = document.createElement('div');
+    element.classList.add(domclass);
+    element.textContent = text;
+    return element;
+}
+
+
+
+
+export {makeElement as makeElement};
+
+
+
